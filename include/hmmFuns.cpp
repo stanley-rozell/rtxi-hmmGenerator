70be1ed4e07fef1cfa41f156521aa54c1a71f2fd
Adam Willats <awillats3@gatech.edu>
2021-05-20 11:28:33 -0400

    small change to generate

--- include/hmmFuns.cpp
+++ include/hmmFuns.cpp
@@ -14,9 +14,9 @@ using namespace std;
 vector<int> genHMM(vector<double> frs, vector<double> trs, int numSteps){
     vector<int> states(numSteps);
     vector<int> spikes(numSteps);
-    
+
     states[0] = 0;
-    
+
     //intialize random number generator
 
     random_device rd;
@@ -26,28 +26,28 @@ vector<int> genHMM(vector<double> frs, vector<double> trs, int numSteps){
 
 
     for (int i=0; i<numSteps; i++){
-        
+
         //get emiss
         //get transit
-        
+
         //spit spikes
 //	double r1 = rand()/double(RAND_MAX);
 
-        spikes[i] = (unif(gen) < frs[states[i]]) ? 1 : 0;
+            spikes[i] = (unif(gen) < frs[states[i]]) ? 1 : 0;
 
 
-        //update future state from current transition probability
-	//NB: this clause is not necessary outside rtxi...
-	if (i<(numSteps-1))
-	{
-        	states[i+1] = (unif(gen) < trs[states[i]]) ? 1-states[i] : states[i];
-	}
-	else
-	{
-		spikes[0]=i;
-	}
+            //update future state from current transition probability
+    	//NB: this clause is not necessary outside rtxi...
+    	if (i<(numSteps-1))
+    	{
+            	states[i+1] = (unif(gen) < trs[states[i]]) ? 1-states[i] : states[i];
+    	}
+    	else
+    	{
+    		spikes[0]=i;
+    	}
     }
 
-    
+
     return spikes;
 }
